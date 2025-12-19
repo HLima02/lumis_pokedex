657437f286a2332b6f8dd5341aa20c2b94072751
HLima02 <hiago.liima02@hotmail.com>
2025-12-19 10:23:52 -0300

    busca feita na lista completa de pokemons

--- js/main.js
+++ js/main.js
@@ -2,30 +2,18 @@ import { fetchPokemon, fetchPokemonDetail } from './api.js'
 import { renderPokemons } from './render.js'
 import { state } from './state.js'
 
-// export async function loadPokemons() {
-//   const offset = (state.currentPage -1) * state.limit
-//   const data = await fetchPokemon(state.limit, offset)
-
-//   const details = await Promise.all(
-//     data.results.map(p => fetchPokemonDetail(p.url))
-//   )
-
-//   //filtro para debounce 
-//   const filtered = details.filter(p => 
-//     p.name.includes(state.search)
-//   )
-
-// }
 
 window.addEventListener('load', async () => {
   fetchPokemon()
   let debounceTimer = null
   const searcInput = document.querySelector('#input_pokemon')
 
-
+  const allList = await fetchPokemon(0, 2000)
   const pokeList = await fetchPokemon()
   let baseList = pokeList.results
+  console.log(allList.results)
 
+  //"Escuta" digitação no campo de input, filtra e faz debounce de resultados
   searcInput.addEventListener('input', (e) => {
     clearTimeout(debounceTimer)
 
@@ -33,8 +21,10 @@ window.addEventListener('load', async () => {
       const search = e.target.value.toLowerCase()
 
       baseList = search !== '' ? (
-        pokeList.results.filter(item => item.name.toLowerCase().includes(search))
+        allList.results.filter(item => item.name.toLowerCase().includes(search))
       ): pokeList.results
+
+      console.log(baseList)
 
       const details = await Promise.all(
         baseList.map(item => fetchPokemonDetail(item.url))
@@ -49,28 +39,5 @@ window.addEventListener('load', async () => {
     baseList.map(item => fetchPokemonDetail(item.url))
   )
 
-  renderPokemons(details)
-
-  
-  // searcInput.addEventListener('input', (e) => {
-  //   clearTimeout(debounceTimer)
-
-  //   debounceTimer = setTimeout(async () => {
-  //     const search = e.target.value.toLowerCase()
-
-  //     const filteredList = search ? (
-  //       baseList.filter(
-  //         item => item.toLowerCase().includes(search)
-  //       )
-  //     ) : baseList
-
-  //     const details = await Promise.all(
-  //       filteredList.map(iitem => fetchPokemonDetail(item.url))
-  //     )
-
-  //   }, 3000)
-  // })
-
- 
-  
+  renderPokemons(details)  
 })
